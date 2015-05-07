478082b2d2569b1aed21e200ffd7275ca42904db
luisgiltajamar <luis.gil@tajamar365.com>
2015-05-07 09:14:52 +0200

    B

---
+++ ChatServerSignalR/Scripts/michat.js
@@ -0,0 +1,56 @@
+﻿var chatHub;
+$(document).ready(function () {
+    chatHub = $.connection.chatHub;
+    registrarEventos();
+
+
+});
+
+function registrarLlamadas() {
+
+    $("#btnRegistrar").click(function() {
+        var nombre = $("#txtLogin").val();
+        chatHub.server.conectar(nombre);
+    });
+
+    $("#btnEnviar").click(function() {
+        var texto = $("#mensaje").val();
+        chatHub.server.enviarMensaje(miNombre, texto);
+        $("#mensaje").val("");
+    });
+
+
+}
+
+
+function registrarEventos() {
+
+    chatHub.client.onConnected = function(id, nombre, usuarios, mensajes) {
+
+
+    };
+    chatHub.client.onNewUserConnected=function(id, nombre) {
+        
+
+
+    };
+
+    chatHub.client.usuarioDesconectado = function(id, nombre) {
+        
+
+
+    };
+
+    chatHub.client.mensaje = function(usuario, mensaje) {
+        
+
+
+    };
+
+    chatHub.client.enviarPrivado = function(id, nombre, mensaje) {
+
+
+    };
+
+}
+
